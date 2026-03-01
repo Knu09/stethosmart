a9281d92c7bc9a3877b6206e6a2066212cb55d00
Knu09 <132640979+Knu09@users.noreply.github.com>
2026-03-01 18:47:47 +0800

    feat (train): implement the train model

--- lw_CNN_model/train.py
+++ lw_CNN_model/train.py
@@ -1,12 +1,17 @@
-import os
+import torch
+import torch.nn as nn
 from torch import full
+from torch.utils.data import TensorDataset, DataLoader
+from lw_cnn_model import LungSoundCNN
+
 import numpy as np
-import librosa
 from preprocessing import (
     segment_cycles,
     load_icbhi_splits,
     load_icbhi_labels,
     build_file_list,
+    build_cycle_dataset,
+    extract_features,
 )
 from typing import List, Dict
 from numpy.typing import NDArray
@@ -42,97 +47,108 @@ def main():
         ICBHI_DATASET_PATH, train_subjs, test_subjs, diag_map
     )
 
-    total_cycles = 0
-    for file, label in train_files:
-        print(f"File Path: {file}")
-        print(f"Label: {label}")
-        cycles = segment_cycles(file)
-        print(f"Length of cycles: {len(cycles)}")
+    # Build cycle-level dataset
+    train_audio, train_labels = build_cycle_dataset(train_files)
+    test_audio, test_labels = build_cycle_dataset(test_files)
 
-        if cycles is not None:
-            total_cycles += len(cycles)
+    # Extract train and test features
+    train_features = [extract_features(c) for c in train_audio]
+    test_features = [extract_features(c) for c in test_audio]
 
-    print(f"Total lenght of cycles: {total_cycles}")
+    # Convert to tensors
+    X_train = torch.tensor(np.array(train_features)).unsqueeze(1).float()
+    y_train = torch.tensor(train_labels).long()
 
+    X_test = torch.tensor(np.array(test_features)).unsqueeze(1).float()
+    y_test = torch.tensor(test_labels).long()
 
-# -----------------------
-# Dataset + Loader
-# -----------------------
-# train_dataset = LungDataset(DATASET_PATH, train_files, label_map)
-# test_dataset = LungDataset(DATASET_PATH, test_files, label_map)
+    # -----------------------
+    # Dataset + Loader
+    # -----------------------
+    train_dataset = TensorDataset(X_train, y_train)
+    test_dataset = TensorDataset(X_test, y_test)
 
-# train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
-# test_loader = DataLoader(test_dataset, batch_size=BATCH_SIZE)
+    train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
+    test_loader = DataLoader(test_dataset, batch_size=BATCH_SIZE)
 
+    # -----------------------
+    # Model
+    # -----------------------
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-# # -----------------------
-# # Model
-# # -----------------------
-# device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-#
-# model = LungSoundCNN(num_classes=4).to(device)
-#
-# criterion = nn.CrossEntropyLoss()
-# optimizer = torch.optim.Adam(model.parameters(), lr=LR)
-#
-#
-# # -----------------------
-# # TRAIN LOOP (100 EPOCHS)
-# # -----------------------
-# for epoch in range(EPOCHS):
-#
-#     model.train()
-#     running_loss = 0
-#     correct = 0
-#     total = 0
-#
-#     for X, y in train_loader:
-#         X, y = X.to(device), y.to(device)
-#
-#         optimizer.zero_grad()
-#         outputs = model(X)
-#
-#         loss = criterion(outputs, y)
-#         loss.backward()
-#         optimizer.step()
-#
-#         running_loss += loss.item()
-#
-#         _, predicted = torch.max(outputs, 1)
-#         total += y.size(0)
-#         correct += (predicted == y).sum().item()
-#
-#     train_acc = 100 * correct / total
-#
-#     # -----------------------
-#     # TEST EVALUATION
-#     # -----------------------
-#     model.eval()
-#     test_correct = 0
-#     test_total = 0
-#
-#     with torch.no_grad():
-#         for X, y in test_loader:
-#             X, y = X.to(device), y.to(device)
-#             outputs = model(X)
-#
-#             _, predicted = torch.max(outputs, 1)
-#             test_total += y.size(0)
-#             test_correct += (predicted == y).sum().item()
-#
-#     test_acc = 100 * test_correct / test_total
-#
-#     print(
-#         f"Epoch [{epoch+1}/{EPOCHS}] "
-#         f"Loss: {running_loss:.4f} "
-#         f"Train Acc: {train_acc:.2f}% "
-#         f"Test Acc: {test_acc:.2f}%"
-#     )
-#
-#
-# # Save model
-# torch.save(model.state_dict(), "lung_model.pth")
-# print("Model saved!")
+    model = LungSoundCNN(num_classes=4).to(device)
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=LR)
+
+    # -----------------------
+    # TRAIN LOOP (100 EPOCHS)
+    # -----------------------
+    for epoch in range(EPOCHS):
+        model.train()
+        running_loss = 0
+        correct = 0
+        total = 0
+
+        for X, y in train_loader:
+            X, y = X.to(device), y.to(device)
+
+            optimizer.zero_grad()
+            outputs = model(X)
+
+            loss = criterion(outputs, y)
+            loss.backward()
+            optimizer.step()
+
+            running_loss += loss.item()
+
+            _, predicted = torch.max(outputs, 1)
+            total += y.size(0)
+            correct += (predicted == y).sum().item()
+
+        train_acc = 100 * correct / total
+
+        # -----------------------
+        # TEST EVALUATION
+        # -----------------------
+        model.eval()
+        test_correct = 0
+        test_total = 0
+
+        with torch.no_grad():
+            for X, y in test_loader:
+                X, y = X.to(device), y.to(device)
+                outputs = model(X)
+
+                _, predicted = torch.max(outputs, 1)
+                test_total += y.size(0)
+                test_correct += (predicted == y).sum().item()
+
+        test_acc = 100 * test_correct / test_total
+
+        print(
+            f"Epoch [{epoch + 1}/{EPOCHS}] "
+            f"Loss: {running_loss:.4f} "
+            f"Train Acc: {train_acc:.2f}% "
+            f"Test Acc: {test_acc:.2f}%"
+        )
+
+    # Save model
+    torch.save(model.state_dict(), "lung_model.pth")
+    print("Model saved!")
+
+    # total_cycles = 0
+    # for file, label in train_files:
+    #     print(f"File Path: {file}")
+    #     print(f"Label: {label}")
+    #     cycles = segment_cycles(file)
+    #     print(f"Length of cycles: {len(cycles)}")
+    #
+    #     if cycles is not None:
+    #         total_cycles += len(cycles)
+    #
+    # print(f"Total lenght of cycles: {total_cycles}")
+
 
 if __name__ == "__main__":
     main()
