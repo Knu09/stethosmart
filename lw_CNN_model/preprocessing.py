22dcdc91b0f2128ff2b86bd1422da07067db57ca
Knu09 <132640979+Knu09@users.noreply.github.com>
2026-03-01 16:17:07 +0800

    feat (preprocessing): add a method to segment into cycles

--- lw_CNN_model/preprocessing.py
+++ lw_CNN_model/preprocessing.py
@@ -1,6 +1,10 @@
 import librosa
 import numpy as np
+from numpy.typing import NDArray
+from scipy.signal import butter, sosfiltfilt
 import matplotlib.pyplot as plt
+import numpy as np
+from typing import List, Dict
 
 # Parameters
 sr = 22050  # sampling rate
@@ -8,6 +12,73 @@ duration = 5.0  # seconds
 n_mels = 128
 n_mfcc = 40
 hop_length = 512
+
+
+def segment_cycles(wav_path: str, sr: int = 22050) -> List[Dict]:
+    """
+    Segment a respiratory recording into cycles using annotation file.
+    Returns a list of dictionaries containing:
+        - audio segment
+        - crackle label
+        - wheeze label
+    """
+
+    txt_path = wav_path.replace(".wav", ".txt")
+    y, _ = librosa.load(wav_path, sr=sr)
+
+    segments = []
+
+    with open(txt_path, "r") as f:
+        for line in f:
+            start, end, crackle, wheeze = line.strip().split()
+
+            start = float(start)
+            end = float(end)
+            crackle = int(crackle)
+            wheeze = int(wheeze)
+
+            start_sample = int(start * sr)
+            end_sample = int(end * sr)
+
+            cycle_audio = y[start_sample:end_sample]
+
+            # Skips below one second cycle
+            if len(cycle_audio) < sr:
+                continue
+
+            segments.append(
+                {"audio": cycle_audio, "crackle": crackle, "wheeze": wheeze}
+            )
+    return segments
+
+
+def apply_bandpass(
+    y: NDArray[np.float64],
+    sr: int,
+    lowcut: float = 100.0,
+    highcut: float = 2000.0,
+    order: int = 4,
+) -> NDArray[np.float64]:
+    """
+    Stable bandpass filter using second-order sections.
+    """
+
+    nyquist = 0.5 * sr
+
+    low = lowcut / nyquist
+    high = highcut / nyquist
+
+    if high >= 1.0:
+        high = 0.999
+
+    if low <= 0.0:
+        low = 0.001
+
+    sos = butter(order, [low, high], btype="band", output="sos")
+
+    filtered = sosfiltfilt(sos, y)
+
+    return filtered.astype(np.float64)
 
 
 def plot_stacked_feature(stacked: np.ndarray):
